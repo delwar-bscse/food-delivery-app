cac610df491436f8e06c7b09caa3a6dc14541155
Md. Delwar Hossain <delwarbscse@gmail.com>
2025-05-15 16:19:46 +0600

    image url

--- src/lib/filePathUrl.js
+++ src/lib/filePathUrl.js
@@ -1,6 +1,13 @@
 export const refactorFileUrl = (url="") => {
-  if(!url) return "";
+  // if(!url) return "";
   const fileUrl = url.startsWith("http") ? url : `${import.meta.env.VITE_BASE_URL}${url}`;
   // console.log("fileUrl : ", fileUrl);
   return fileUrl;
 }
+
+// export const refactorFileUrl = (url = "") => {
+//   if (!url) return "";
+//   const baseUrl = import.meta.env.VITE_BASE_URL || "";
+//   const fileUrl = url.startsWith("http") ? url : `${baseUrl}${url}`;
+//   return fileUrl;
+// }
