39c800673538612d190047ecc566afe6a7e94e6a
Md. Delwar Hossain <delwarbscse@gmail.com>
2025-08-11 11:40:59 +0600

    domain

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
 import react from "@vitejs/plugin-react";
+
 import { defineConfig } from "vite";
 
 export default defineConfig({
+
   plugins: [react()],
+
   server: {
+
     host: "72.167.54.115",
-  }
+
+    allowedHosts: [
+
+      "www.admin.deliverlydashboard.com",
+
+      "localhost",
+
+      "0.0.0.0"
+
+    ],
+
+  },
+
 });
